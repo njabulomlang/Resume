ece00e6fb0534ca0ca12dc91c7129970b910b1fa
Njabulo96 <njabulomlang96@gmail.com>
2019-06-04 14:08:45 +0200

    resume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [ {path:'contact-me', component: ContactMeComponent},
+{path:'about' , component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import {RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactMeComponent,
+    AboutComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    HttpClientModule,
+    RouterModule.forRoot([{path:'', component: AboutComponent},
+      {path:'contact-me', component: ContactMeComponent},
+    {path:'about' , component: AboutComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
